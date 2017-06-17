ec31dc4dec0d9f12b35227773101f04e88c7ba62
James Pettit <etherealmachine@gmail.com>
2017-06-17 14:11:42 -0700

    Fix bug in period start signal - Check if ready function ran so it only gets run once.

--- otree_redwood/abstract_views.py
+++ otree_redwood/abstract_views.py
@@ -50,6 +50,8 @@ class Page(oTreePage):
                     id_in_subsession=self.group.id_in_subsession,
                     session=self.session
                 )
+            if ready.ran:
+                return
             for player in self.group.get_players():
                 try:
                     Connection.objects.get(participant_code=player.participant.code)
