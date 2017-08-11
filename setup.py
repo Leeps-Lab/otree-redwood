71008f73aba064b1ede029625e4f764a06021881
LEEPS Lab <leepslab@gmail.com>
2017-08-11 12:07:45 -0700

    Update version to 0.2, uploaded to PyPI.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='otree-redwood',
-    version='0.1.3',
+    version='0.2',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
@@ -36,6 +36,6 @@ setup(
         'channels>=1.1.3,<2',
         'daphne>=1.0.0,<2',
         'jsonfield>=2,<3',
-        'mockredispy>=2.9<3',
+        'mockredispy>=2.9.0,<3',
     ]
 )
